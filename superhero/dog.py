44ae1d8b5b3be082179c45baf66255f80d3c10ca
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-05-23 13:57:14 -0700

    added 2 more methods

--- superhero/dog.py
+++ superhero/dog.py
@@ -11,6 +11,17 @@ class Dog:
         """
         print("{} says Woof!".format(self.name))
 
+    def sit(self):
+        """
+        Dog sits when called
+        """
+        print("{} is sitting now".format(self.name))
+
+    def roll_over(self):
+        """
+        Dog Rolls over when called
+        """
+        print("{} is rolling over".format(self.name))
 
 
 
--- superhero/my-dogs.py
+++ superhero/my-dogs.py
@@ -1,7 +1,11 @@
 from dog import Dog
 
 
-my_dog = Dog("Rex", "SuperDog")
-my_dog.bark()
+# my_dog = Dog("Rex", "SuperDog")
+# my_dog.bark()
+
+other_dog = Dog("Annie", "Cutie")
+print(other_dog.name)
+
 
 
