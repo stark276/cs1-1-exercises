790ffa50db1c3b1acb5bb128c4ca38d2ef61609d
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-07-09 16:40:03 -0700

    init test passed

--- superhero/test_superheroes.py
+++ superhero/test_superheroes.py
@@ -1,2 +1,12 @@
 import unittest
+from superheroes import Hero, Ability
 
+
+class HeroTest(unittest.TestCase):
+    
+    def test_init(self):
+        hero = Hero("Superman")
+        assert hero.name is "Superman"
+        assert hero.starting_health == 100
+        assert hero.current_health == hero.starting_health
+        assert hero.abilities == []
